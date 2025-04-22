8c3edb9bc4e377bbf56d226e4eaef0f21135656e
gabipayan <gapayan26@gmail.com>
2025-04-21 21:39:02 -0600

    Add database models for users, clients, and appointments

---
+++ server/models/index.ts
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+import userModel from './user.model';
+import clientModel from './client.model';
+import appointmentModel from './appointment.model';
+
+const sequelize = new Sequelize(process.env.DB_URI as string);
+
+const db: any = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Models
+db.User = userModel(sequelize, Sequelize.DataTypes);
+db.Client = clientModel(sequelize, Sequelize.DataTypes);
+db.Appointment = appointmentModel(sequelize, Sequelize.DataTypes);
+
+// Relationships
+db.Appointment.belongsTo(db.Client, { foreignKey: 'clientId' });
+db.Client.hasMany(db.Appointment, { foreignKey: 'clientId' });
+
+db.Appointment.belongsTo(db.User, { foreignKey: 'professionalId' });
+db.User.hasMany(db.Appointment, { foreignKey: 'professionalId' });
+
+export default db;
