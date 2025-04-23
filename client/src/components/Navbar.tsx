ca0b3e490a0728ab0cfc64549d868a9965e647d7
Ervey Garcia <ervey.garcia@carrier.com>
2025-04-23 11:56:46 -0600

    "Persona 4: Navbar UI translated to English"

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Navbar.css'; // 👈 Importa los estilos
+import './Navbar.css';
 
 const Navbar: React.FC = () => {
   const location = useLocation();
 
-  // Oculta en login
+  // Hide Navbar on login page
   if (location.pathname === '/' || location.pathname === '/login') return null;
 
   return (
@@ -15,9 +15,9 @@ const Navbar: React.FC = () => {
         {location.pathname !== '/dashboard' && (
           <Link to="/dashboard">Home</Link>
         )}
-        <Link to="/schedule">Citas</Link>
-        <Link to="/booking">Agendar</Link>
-        <Link to="/login">Salir</Link>
+        <Link to="/schedule">Schedule</Link>
+        <Link to="/booking">Book</Link>
+        <Link to="/login">Logout</Link>
       </div>
     </nav>
   );
