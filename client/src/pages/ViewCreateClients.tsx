afc5a785bdaeba3c9e24a12828607e91c2db8d45
[William Duarte] <[willi_dds3@hotmail.com]>
2025-04-24 16:24:34 -0600

    Basic Structure for ViewCreateClient component

---
+++ client/src/pages/ViewCreateClients.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import ClientForm from '../components/ClientForm.js'
+
+const ViewCreateClient = () => {
+    return (
+        <div style= {{padding: '2rem'}}>
+            <h1>New Client</h1>
+            <ClientForm/>
+        </div>
+    )
+}
+
+export default ViewCreateClient
+
