dcc6772fd963c503ea496bc23949e3a3589f33ec
[William Duarte] <[willi_dds3@hotmail.com]>
2025-04-23 19:30:23 -0600

    updating all routes from our page

--- server/src/routes/api/user.routes.ts
+++ server/src/routes/api/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { getProfile, updateProfile } from '../../../controllers/user.controller';
-import { verifyToken } from '../../utils/authMiddleware';
+import { getProfile, updateProfile } from '../../controllers/user.controller.js';
+import { verifyToken } from '../../utils/authMiddleware.js';
 
 const router = Router();
 
