0b6f62db6d2860870c7af711bc1a25508f2b4885
[William Duarte] <[willi_dds3@hotmail.com]>
2025-04-27 14:23:09 -0600

    Cleaning code

--- server/src/models/client.model.ts
+++ server/src/models/client.model.ts
@@ -57,39 +57,3 @@ export function ClientFactory(sequelize: Sequelize): typeof Client {
     );
     return Client;
 }
-// =======
-// import { DataTypes, Model } from 'sequelize';
-// import { sequelize } from '../config/db';
-
-// class Client extends Model {
-//   public id!: number;
-//   public name!: string;
-//   public email!: string;
-// }
-
-// Client.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//     },
-//   },
-//   {
-//     sequelize,
-//     modelName: 'Client',
-//     tableName: 'clients',
-//   }
-// );
-
-// export { Client };
-
